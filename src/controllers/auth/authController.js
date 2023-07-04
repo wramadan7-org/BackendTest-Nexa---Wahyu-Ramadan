1faff610f190eaab92569c1ea0947ae856ccee49
Wahyu Ramadan <wramadan1203@gmail.com>
2023-07-05 02:19:25 +0700

    sample response login

--- src/controllers/auth/authController.js
+++ src/controllers/auth/authController.js
@@ -1,22 +1,34 @@
+require("dotenv").config();
 const httpStatus = require("http-status");
+const jwt = require("jsonwebtoken");
 const { loginService } = require("../../services/auth/authService");
+const { compared } = require("../../helpers/bcryptHelper");
+const CustomError = require("../../config/customError");
 
-const loginController = async (req, res) => {
+const { JWT_SECRET } = process.env;
+
+const loginController = async (req, res, next) => {
   const { username, password } = req.body;
 
   try {
-    const result = await loginService(username, password);
+    // Get data admin by username
+    const validUsername = await loginService(username);
+    // Check valid username
+    if (!validUsername || !validUsername.length)
+      return res.sendWrapped("Username incorrect", httpStatus.UNAUTHORIZED);
+    // Compare password from db with request
+    const validPassword = await compared(password, validUsername[0].password);
+    // Check password match
+    if (!validPassword)
+      return res.sendWrapped("Password incorrect", httpStatus.UNAUTHORIZED);
 
-    res.send({
-      status: 200,
-      message: "Success",
-      data: result,
-    });
+    const response = {
+      token: "TOKEN",
+    };
+
+    res.sendWrapped("Login successfully", httpStatus.OK, response);
   } catch (error) {
-    res.send({
-      status: 500,
-      message: error,
-    });
+    next(error);
   }
 };
 
