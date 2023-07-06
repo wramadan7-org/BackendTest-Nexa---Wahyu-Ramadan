3232e3610df329fe711ffc9f29dda755e4ac5c62
Wahyu Ramadan <wramadan1203@gmail.com>
2023-07-06 08:39:05 +0700

    decrypt aes helper

--- src/helpers/aesHelper.js
+++ src/helpers/aesHelper.js
@@ -1,23 +1,34 @@
 require("dotenv").config();
 const CryptoJs = require("crypto-js");
 const CustomError = require("../config/customError");
-const httpStatus = require("http-status");
 
 const { AES_SECRET } = process.env;
 
-const encryptAES = (data) => {
+const encryptAES = async (data) => {
   try {
-    const encrypted = CryptoJs.AES.encrypt(
+    const encrypted = await CryptoJs.AES.encrypt(
       JSON.stringify(data),
       AES_SECRET
     ).toString();
 
     return encrypted;
   } catch (error) {
-    throw new CustomError(error, httpStatus.CONFLICT);
+    throw new CustomError(error, 409);
+  }
+};
+
+const decryptAES = async (encrypt) => {
+  try {
+    const bytes = await CryptoJs.AES.decrypt(encrypt, AES_SECRET);
+    const decrypt = await bytes.toString(CryptoJs.enc.Utf8);
+
+    return decrypt;
+  } catch (error) {
+    throw new CustomError(error, 409);
   }
 };
 
 module.exports = {
   encryptAES,
+  decryptAES,
 };
